39f04fa690fb9ffa63daaadeb3c743135c38bdba
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-07-13 02:41:26 +0300

    'this' with DOM

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,8 @@
-class MyClass {
-    myMethod() {
-        const foo = 123;
-        console.log('1', this);
-        setTimeout(() => {
-            // lexical scope
-            console.log(this); // --> MyClass. Doesn't create scope change 
-        }, 0);
-        setTimeout(function() {
-            console.log(this); // --> Window
-        }, 0);
-    }
-    foo(){
-        const foo = 456;
-        console.log('foo', foo);
-    }
+const elem = document.querySelector('.click');
+
+function handleClick(this: HTMLAnchorElement, event: Event) {
+    event.preventDefault();
+    console.log(this.classList);
 }
 
-const myInstance = new MyClass();
-myInstance.myMethod();
+elem.addEventListener('click', handleClick, false);
