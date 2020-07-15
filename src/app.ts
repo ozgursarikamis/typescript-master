fcdab35603c9acc1b3e59f817e3d8b6b8fe2ca09
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-07-16 01:52:21 +0300

    “Required” Mapped Type, +/- Modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,21 @@
 interface Person {
     name: string;
-    age: number;
-}
-
-interface PartialPerson {
-    name?: string;
     age?: number;
 }
-// type MyPartial<T> = {
-//     [P in keyof T]?: T[P];
-// }
-function updatePerson(person: Person, prop: Partial<Person>) {
-    return { ...person, ...prop };
-}
-interface readonlyPerson {
-    readonly name: string;
-    readonly age: number;
+
+type MyRequired<T> = {
+    //    readonly [P in keyof T]?: T[P] // any of them are optional
+    //    readonly [P in keyof T]+?: T[P] // completely same
+    [P in keyof T]-?: T[P]
 }
 
-const person: Person = {
+function printAge(person: Required<Person>) {
+    return `${person.name} is ${person.age}`; 
+}
+
+const person: Required<Person> = {
     name: 'Özgür',
-    age: 27
+    age: 30,    
 };
-const updatedPerson = updatePerson(person, { name: 'ABC'});
-person.name = 'Özgür';
-person.age = 27;
 
-type MyReadonly<T>= {
-    readonly [P in keyof T]: T[P]
-}
-function freezePerson<T>(person: T): MyReadonly<T> {
-    return Object.freeze(person);
-}
-
-const newPerson = freezePerson(person);
-newPerson.age = 27;
+const age = printAge(person);
