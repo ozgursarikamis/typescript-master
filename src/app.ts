f228fa3cb76395479c1d79c2c9b27f71213c4804
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-07-18 01:57:34 +0300

    Interfaces vs Type Aliases

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,16 @@
-interface Order {
-    id: string;
-    amount: number;
-    currency: string;
-}
+interface Item { name: string; }
 
-interface Stripe {
-    type: 'stripe';
-    card: string;
-    cvc: string;
-}
+interface Artist extends Item { songs: number; }
+// merging interfaces, which we can't do with types
+interface Artist { getSongs(): number }
 
-interface PayPal {
-    type: 'paypal';
-    email: string;
-}
+// instead of extend, we can use intersection type:
+type Artist2 = { name: string } & Item;
 
-type CheckoutCard = Order & Stripe; // intersection type
-type CheckoutPayPal = Order & PayPal;
-type CheckoutABC = Order & { name: string };
-
-const order: Order = {
-    id: 'xyz123',
-    amount: 100,
-    currency: 'USD'
-};
-
-const orderCard: CheckoutCard = {
-    ...order,
-    type: 'stripe',
-    card: '1000 2000 3000 4000',
-    cvc: '123'
-};
-
-const orderPayPal: CheckoutPayPal = {
-    ...order, email: 'abc@mail.com', type: 'paypal'
-};
-
-// ES equivalent:
-const assigned = Object.assign({}, order, orderCard);
-
-type Payload = CheckoutCard | CheckoutPayPal;
-
-function checkout(payload: Payload) {
-    if (payload.type === 'stripe') {
-        console.log(payload.card, payload.cvc);
-    }
-    if (payload.type === 'paypal') {
-        console.log(payload.email);
-    }
-    // or:
-    switch (payload.type) {
-        case "paypal":
-            console.log(payload.email);
-            break;
-        case "stripe":
-            console.log(payload.card, payload.cvc);
-            break;
+const newArtist: Artist = {
+    name: 'abc',
+    songs: 5,
+    getSongs(): number {
+        return this.songs;
     }
 }
