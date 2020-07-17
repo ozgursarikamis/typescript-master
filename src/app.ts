3e1b399789305215aa2f37fdbcbac3e354230e62
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-07-17 03:30:03 +0300

    Literal Type Guards and “in” Operator

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
+// const exists = 'localStorage' in window;
+// for (const prop in [{}]) {}
+
+const foo = 'bar'; // literal type
+
 class Song {
+    kind:'song'; // declaring only type of 'kind' as 'song'
     constructor(public title: string, public duration: number) { }
 }
 class PlayList {
+    kind: 'playlist'; // declaring only type of 'kind' as 'playlist'
     constructor(public name: string, public songs: Song[]) { }
 }
 
 function isSong(item: any): item is Song { // explicitly declare the return type to not to get compile-time errors.
-    return item instanceof Song;
+    return 'title' in item;
 }
+
 function getItemName(item: Song | PlayList) {
-    if (isSong(item)) {
-        return (item as Song).title;
+    // if (isSong(item)) {
+    //     return (item as Song).title;
+    // }
+    if (item.kind === 'song') {
+        return item.title;
     }
     return item.name;
 }
