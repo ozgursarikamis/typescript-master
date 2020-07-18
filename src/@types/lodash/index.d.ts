972f77f43a74a6d3ce9058cd21128166b7f08124
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-07-18 03:20:45 +0300

    Augmenting Modules with Declarations

--- src/@types/lodash/index.d.ts
+++ src/@types/lodash/index.d.ts
@@ -1,5 +1,9 @@
 // d in the file name stands for declarations
 
+import * as lodash from "lodash";
+
 declare module 'lodash' {
-    export function chunk(collection: any, size?: number): any[][];
+    interface LoDashStatic {
+        log(item: string): void;
+    }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
-// Writing Declaration Files
+// Augmenting Modules with Declarations
 
 import * as _ from "lodash";
 
 _.chunk([1, 2, 3, 4, 5], 2); // [1,2], [3,4]
+
+_.mixin({
+    log(item: string){ console.log(':::', item); }
+});
+
+_.log('Hello!');
+_.log(123);
